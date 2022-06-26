631603bcaf8dc576ae078d68e33879bd42e4ea78
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-26 01:31:16 -0300

    Adiciona migration com tabelas do identity

--- src/backend/CS.Data/CsDbContext.cs
+++ src/backend/CS.Data/CsDbContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CS.Domain.Entidades;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace CS.Data
 {
-    public class CsDbContext : DbContext
+    public class CsDbContext : IdentityDbContext<Usuario, Perfil, Guid, UsuarioPermissao, UsuarioPerfil, UsuarioLogin, PerfilPermissao,
+        UsuarioToken>
     {
         public CsDbContext()
         {
@@ -17,5 +21,13 @@ namespace CS.Data
             base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(typeof(CsDbContext).Assembly);
         }
+
+        public DbSet<Perfil> Perfil { get; set; }
+        public DbSet<PerfilPermissao> PerfilPermissao { get; set; }
+        public DbSet<UsuarioLogin> UsuarioLogin { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<UsuarioPerfil> UsuarioPerfil { get; set; }
+        public DbSet<UsuarioPermissao> UsuarioPermissao { get; set; }
+        public DbSet<UsuarioToken> UsuarioToken { get; set; }
     }
 }
