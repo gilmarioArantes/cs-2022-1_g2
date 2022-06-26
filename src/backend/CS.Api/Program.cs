6bb19cd7435d573443073f33a1a3eace699027b8
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-26 01:34:27 -0300

    Registra services em Program

--- src/backend/CS.Api/Program.cs
+++ src/backend/CS.Api/Program.cs
@@ -1,5 +1,9 @@
+using CS.Application.Interface;
+using CS.Application.Services;
 using CS.Core.Notificador;
 using CS.Data;
+using CS.Domain.Entidades;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +11,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDataProtection();
+
+builder.Services.AddIdentity<Usuario, Perfil>(config =>
+    {
+        config.Password.RequireUppercase = false;
+        config.Password.RequireDigit = false;
+        config.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<CsDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddDbContext<CsDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<INotificador, Notificador>();
+builder.Services.AddScoped<Seeder>();
+builder.Services.AddScoped<IAutenticacaoService, AutenticacaoService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
+    seeder.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
