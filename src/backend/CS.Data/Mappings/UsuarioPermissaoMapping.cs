5b2d290071e5b40c1dc38669b344c2dcd8fd9132
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-26 01:30:24 -0300

    Adiciona classe UsuarioPermissaoMapping

---
+++ src/backend/CS.Data/Mappings/UsuarioPermissaoMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CS.Data.Mappings
+{
+    public class UsuarioPermissaoMapping : IEntityTypeConfiguration<UsuarioPermissao>
+    {
+        public UsuarioPermissaoMapping()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<UsuarioPermissao> builder)
+        {
+            builder.ToTable("UsuarioPermissao", "Identidade");
+        }
+    }
+}
