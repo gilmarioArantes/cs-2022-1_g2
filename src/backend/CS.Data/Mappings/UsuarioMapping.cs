fa97fcfc1ea61fdaccdb732f1901690965d41027
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-26 01:30:04 -0300

    Adiciona classe UsuarioMapping

---
+++ src/backend/CS.Data/Mappings/UsuarioMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CS.Data.Mappings
+{
+    public class UsuarioLoginMapping : IEntityTypeConfiguration<UsuarioLogin>
+    {
+        public UsuarioLoginMapping()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<UsuarioLogin> builder)
+        {
+            builder.ToTable("UsuarioLogin", "Identidade");
+        }
+    }
+}
