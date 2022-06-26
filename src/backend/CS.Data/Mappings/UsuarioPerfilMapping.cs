d3ad3cc2c0802024cc1d13f57b17e3ef38843f12
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-26 01:30:14 -0300

    Adiciona classe UsuarioPerfilMapping

---
+++ src/backend/CS.Data/Mappings/UsuarioPerfilMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CS.Data.Mappings
+{
+    public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMapping()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario", "Identidade");
+
+            builder.HasMany(usuario => usuario.UsuarioPerfis);
+        }
+    }
+}
