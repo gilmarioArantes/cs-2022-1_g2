0d39918b6caaa3be447fe82e0ac9331ea0ae997d
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-25 22:38:01 -0300

    Adiciona Notificador e implementa metodo CustomResponse em BaseController

--- src/backend/CS.Api/Controllers/BaseController.cs
+++ src/backend/CS.Api/Controllers/BaseController.cs
@@ -1,9 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CS.Core.Notificador;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CS.Api.Controllers
 {
     [ApiController]
     public abstract class BaseController : Controller
     {
+        private readonly INotificador _notificador;
+
+        protected BaseController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (!PossuiNotificacao())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterErros()
+            });
+        }
+
+        private bool PossuiNotificacao()
+        {
+            return _notificador.PossuiNotificacao();
+        }
     }
 }
--- src/backend/CS.Api/Program.cs
+++ src/backend/CS.Api/Program.cs
@@ -1,8 +1,12 @@
+using CS.Core.Notificador;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<INotificador, Notificador>();
 
 var app = builder.Build();
 
