9b58f64965b49aeec7832803f4cdf3ffd43d68b4
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-25 22:58:34 -0300

    Adiciona CsDbContext

---
+++ src/backend/CS.Data/CsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CS.Data
+{
+    public class CsDbContext : DbContext
+    {
+        public CsDbContext()
+        {
+        }
+
+        public CsDbContext(DbContextOptions<CsDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(CsDbContext).Assembly);
+        }
+    }
+}
