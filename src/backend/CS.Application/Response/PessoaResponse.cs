423372659c7155c87d9f5a402ab7abe302a00a05
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-26 03:18:59 -0300

    Adiciona PessoaResponse

---
+++ src/backend/CS.Application/Response/PessoaResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace CS.Application.Response
+{
+    public abstract class PessoaResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Email { get; set; }
+    }
+}
