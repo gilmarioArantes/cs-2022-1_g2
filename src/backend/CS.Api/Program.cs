26c920d52ac34c200344e99ba00162493e0fdfa9
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-06-25 22:59:12 -0300

    Realiza setup para conexao com banco de dados postgres

--- src/backend/CS.Api/Program.cs
+++ src/backend/CS.Api/Program.cs
@@ -1,4 +1,6 @@
 using CS.Core.Notificador;
+using CS.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<CsDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<INotificador, Notificador>();
 
 var app = builder.Build();
