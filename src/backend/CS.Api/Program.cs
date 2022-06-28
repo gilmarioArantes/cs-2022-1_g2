810ee3117e05bd49ec6c78b58bfda1b25f079b76
vitorhenrique-ufg <vitorhfbc2@hotmail.com>
2022-06-27 22:07:06 -0300

    Ajusta configuracao para razor page

--- src/backend/CS.Api/Program.cs
+++ src/backend/CS.Api/Program.cs
@@ -1,6 +1,8 @@
 using CS.Api.Extensions;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -8,7 +10,9 @@ builder.Services.AddDataProtection();
 builder.Services.AddIdentityConfiguration();
 builder.Services.AddSwaggerConfigurations();
 builder.Services.RegisterServices(builder.Configuration);
+builder.Services.AddRazorPages();
 
+//builder.Services.AddScoped<IAuthorizationMiddlewareResultHandler, RequestAuthorizationMiddlewareResultHandler>();
 var app = builder.Build();
 app.Migrate();
 
@@ -23,12 +27,20 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 
 app.UseAuthorization();
+app.UseStaticFiles();
 
 app.UseCors(x => x
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader());
 
+app.MapRazorPages();
 app.MapControllers();
+
+app.MapGet("/Sair", (HttpContext context) =>
+{
+    context.Response.Cookies.Delete("token-jwt");
+    context.Response.Redirect($"/login");
+});
 
 app.Run();
