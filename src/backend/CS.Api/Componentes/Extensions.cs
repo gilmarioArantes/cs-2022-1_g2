bd6979aeb54eb270843cd03d766975da0ce62cba
vitorhenrique-ufg <vitorhfbc2@hotmail.com>
2022-06-27 22:05:06 -0300

    Adiciona componente de grid.

---
+++ src/backend/CS.Api/Componentes/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using CS.Api.Componentes.Builders.Grid;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq.Expressions;
+
+namespace CS.Api.Componentes
+{
+    public static class Extensions
+    {
+        public static GridOptionsBuilder<TModel, TProperty> GridOptions<TModel, TProperty>(
+        this IHtmlHelper<TModel> _, Expression<Func<TModel, IEnumerable<TProperty>>> expression)
+        where TModel : class
+        where TProperty : class
+        {
+            return new GridOptionsBuilder<TModel, TProperty>().Init(expression);
+        }
+    }
+}
