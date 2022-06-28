36347ae1c2927f3973f8f810f639168458d82067
vitorhenrique-ufg <vitorhfbc2@hotmail.com>
2022-06-27 22:05:20 -0300

    Adiciona extensao de componentes

---
+++ src/backend/CS.Api/Componentes/Utilidades/UtilidadesComponentes.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace CS.Api.Componentes.Utilidades
+{
+    public static class UtilidadesComponentes
+    {
+        public static string ObtenhaDisplayName<TModel, TProperty>(Expression<Func<TModel, TProperty>> expression)
+        {
+            MemberInfo member = ((MemberExpression)expression.Body).Member;
+
+            DisplayAttribute atributoDisplay = (DisplayAttribute)member
+                .GetCustomAttributes(typeof(DisplayAttribute), false)
+                .FirstOrDefault();
+
+            return atributoDisplay?.Name ?? member.Name;
+        }
+
+        public static string ObtenhaName<TModel, TProperty>(Expression<Func<TModel, TProperty>> expression)
+        {
+            string bodyExpressao = expression.Body.ToString();
+            int posicaoAcessoMembro = bodyExpressao.IndexOf(".", StringComparison.Ordinal);
+            string bodySemNomeParametro = bodyExpressao.Substring(++posicaoAcessoMembro);
+
+            return Regex.Replace(bodySemNomeParametro, @"\(|\)", "");
+        }
+    }
+}
