a53660c79ae110a38c141204856b86809fe661fc
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-07-16 21:17:49 -0300

    Adiciona Peso e Altura em Aluno

--- src/CS.Domain/Entidades/Aluno.cs
+++ src/CS.Domain/Entidades/Aluno.cs
@@ -5,11 +5,16 @@
         public Guid UsuarioId { get; set; }
         public virtual Usuario Usuario { get; set; }
 
-        public void Atualizar(string cpf, DateTime dataNascimento, string nome)
+        public decimal Peso { get; set; }
+        public decimal Altura { get; set; }
+
+        public void Atualizar(string cpf, DateTime dataNascimento, string nome, decimal peso, decimal altura)
         {
             Nome = nome;
             Cpf = cpf;
             DataNascimento = dataNascimento;
+            Peso = peso;
+            Altura = altura;
         }
     }
 }
