fa1de8b2d24ceba58125a5fe5c83cff7743056f7
vitorhenrique-ufg <vitorhfbc2@hotmail.com>
2022-07-20 17:47:07 -0300

    Adiciona migration de exercicio.

---
+++ src/CS.Data/Migrations/20220720015327_Exercicio.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CS.Data.Migrations
+{
+    public partial class Exercicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Altura",
+                schema: "Sistema",
+                table: "Aluno",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Peso",
+                schema: "Sistema",
+                table: "Aluno",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Exercicio",
+                schema: "Sistema",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TipoExercicio = table.Column<int>(type: "integer", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: true),
+                    Restricao = table.Column<string>(type: "text", nullable: true),
+                    GrupoCorporal = table.Column<int>(type: "integer", nullable: false),
+                    QuantidadeRepeticao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercicio", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercicio",
+                schema: "Sistema");
+
+            migrationBuilder.DropColumn(
+                name: "Altura",
+                schema: "Sistema",
+                table: "Aluno");
+
+            migrationBuilder.DropColumn(
+                name: "Peso",
+                schema: "Sistema",
+                table: "Aluno");
+        }
+    }
+}
